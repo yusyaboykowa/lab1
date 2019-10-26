4e1ce69797a45cbc26f8c5e240f9bb5816f6ca2a
yusyaboykowa <55316927+yusyaboykowa@users.noreply.github.com>
2019-10-26 13:18:16 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,7 +1,21 @@
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjklmnopqrstuvwxyzabcdefghjklmnopqrstuvwxyz01234567890123456789АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїклмнопрстуфхцчшщьюя"
-step = 1
-text = input("Please write your text ").strip()
-res = ''
-for c in text:
-    res += alphabet[(alphabet.index(c) + step)%len(alphabet)]
-print("Result: " + res + "")
+i = True
+while i:
+    alpha = ' abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM0123456789'
+    step = 2
+    step1 = 1
+    text = input("Please write your text ").strip()
+    res = ''
+    digit=''
+    for c in text:
+        if c.isalpha():
+          if digit:
+            res += str(int(digit)+int(step1))
+            digit = ''
+          res += alpha[(alpha.index(c) + step) % len(alpha)]
+        elif c.isnumeric():
+          digit += c
+        else:
+            res += c
+    if digit:
+      res += str(int(digit)+int(step1))
+    print("Result: " + res + "")
