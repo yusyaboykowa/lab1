5618833bc4a14ed981ed91341338a5a41eca2512
yusyaboykowa <55316927+yusyaboykowa@users.noreply.github.com>
2019-10-26 11:21:03 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,12 +1,7 @@
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjklmnopqrstuvwxyzabcdefghjklmnopqrstuvwxyz01234567890123456789"
-a=input("Enter your sentence:")
-key=int(input("Enter your key:"))
-b=""
-for letter in a:
-    position=alphabet.find(letter)
-    newPosition=position+key
-    if letter in alphabet:
-        b=b+alphabet[newPosition]
-    else:
-        b=b+letter
-print("Your sentence:", b)
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjklmnopqrstuvwxyzabcdefghjklmnopqrstuvwxyz01234567890123456789АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїклмнопрстуфхцчшщьюя"
+step = 1
+text = input("Please write your text ").strip()
+res = ''
+for c in text:
+    res += alphabet[(alphabet.index(c) + step)%len(alphabet)]
+print("Result: " + res + "")
